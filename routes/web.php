93a0e9eebc35915a805809e30b6552dd92e00280
Johann <johann.fazakas@pearson.com>
2021-01-22 19:44:13 +0200

    added auth middleware to endpoints that should be protected

--- routes/web.php
+++ routes/web.php
@@ -27,57 +27,68 @@ Route::prefix('/shop')->group(function () {
 
     Route::get('/products', [
         'uses' => 'App\Http\Controllers\ShopController@products',
-        'as' => 'shop.products'
+        'as' => 'shop.products',
+        'middleware' => 'auth'
     ]);
 
     Route::get('/cart', [
         'uses' => 'App\Http\Controllers\ShopController@cart',
-        'as' => 'shop.cart'
+        'as' => 'shop.cart',
+        'middleware' => 'auth'
     ]);
 
     Route::get('/orders', [
         'uses' => 'App\Http\Controllers\ShopController@orders',
-        'as' => 'shop.orders'
+        'as' => 'shop.orders',
+        'middleware' => 'auth'
     ]);
 
     Route::get('/product/{id}', [
         'uses' => 'App\Http\Controllers\ShopController@product',
-        'as' => 'shop.product'
+        'as' => 'shop.product',
+        'middleware' => 'auth'
     ]);
 
     Route::get('/create', [
         'uses' => 'App\Http\Controllers\ShopController@create',
-        'as' => 'shop.create'
+        'as' => 'shop.create',
+        'middleware' => 'auth'
     ]);
 
     Route::get('/update/{id}', [
         'uses' => 'App\Http\Controllers\ShopController@update',
-        'as' => 'shop.update'
+        'as' => 'shop.update',
+        'middleware' => 'auth'
     ]);
 
     Route::get('/delete/{id}', [
         'uses' => 'App\Http\Controllers\ShopController@delete',
-        'as' => 'shop.delete'
+        'as' => 'shop.delete',
+        'middleware' => 'auth'
     ]);
 
     Route::get('/placeOrder', [
         'uses' => 'App\Http\Controllers\ShopController@placeOrder',
-        'as' => 'shop.placeOrder'
+        'as' => 'shop.placeOrder',
+        'middleware' => 'auth'
     ]);
 
     Route::post('/addToCart', [
         'uses' => 'App\Http\Controllers\ShopController@addToCart',
-        'as' => 'shop.addToCart'
+        'as' => 'shop.addToCart',
+        'middleware' => 'auth'
     ]);
 
     Route::post('/createProduct', [
         'uses' => 'App\Http\Controllers\ShopController@createProduct',
-        'as' => 'shop.createProduct'
+        'as' => 'shop.createProduct',
+        'middleware' => 'auth'
     ]);
 
     Route::post('/updateProduct', [
         'uses' => 'App\Http\Controllers\ShopController@updateProduct',
-        'as' => 'shop.updateProduct'
+        'as' => 'shop.updateProduct',
+        'middleware' => 'auth'
     ]);
 
 });
