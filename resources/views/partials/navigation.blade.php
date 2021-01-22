684ef644b01ac115cdf18f0018b54eab54360be9
Johann <johann.fazakas@pearson.com>
2021-01-22 20:08:00 +0200

    template options visible based on role

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -6,9 +6,14 @@
         <li><a href="{{ route('register') }}">Sign up</a></li>
         @else
         <li><a href="{{ route('shop.products') }}">Products</a></li>
+
+        @if(!Auth::user()->is_admin)
         <li><a href="{{ route('shop.cart') }}">Cart</a></li>
+        <li><a href="{{ route('shop.orders') }}">Orders</a></li>
+        @endif
+
         <li><a class="dropdown-item" href="{{ route('logout') }}"
-                onclick="event.preventDefault();
+               onclick="event.preventDefault();
                 document.getElementById('logout-form').submit();">
                 {{ __('Logout') }}
             </a>
@@ -16,7 +21,7 @@
                 @csrf
             </form>
         </li>
-        <li><a href="{{ route('shop.orders') }}">Orders</a></li>
+
         @endif
     </ul>
 </div>
--- resources/views/shop/product.blade.php
+++ resources/views/shop/product.blade.php
@@ -12,13 +12,17 @@
     <p>Price: {{$product->price}}</p>
     <p>Quantity: {{$product->quantity}}</p>
     <p>Date Added: {{$product->created_at}}</p>
-    <form action="{{ route('shop.addToCart') }}" method="POST">
-        @csrf
-        <label>Quantity: </label><input type="number" name="quantity">
-        <input type="hidden" name="id" value="{{$product->id}}">
-        <button type="submit">add to cart</button>
-    </form>
-    <a href="{{ route('shop.update', ['id' => $product->id]) }}">Update product</a>
-    <a href="{{ route('shop.delete', ['id' => $product->id]) }}">Delete product</a>
+
+    @if(Auth::user()->is_admin)
+        <a href="{{ route('shop.update', ['id' => $product->id]) }}">Update product</a>
+        <a href="{{ route('shop.delete', ['id' => $product->id]) }}">Delete product</a>
+    @else
+        <form action="{{ route('shop.addToCart') }}" method="POST">
+            @csrf
+            <label>Quantity: </label><input type="number" name="quantity">
+            <input type="hidden" name="id" value="{{$product->id}}">
+            <button type="submit">add to cart</button>
+        </form>
+    @endif
 </div>
 @endsection
